ac5f484416d67c0f0bf5d854fddc11c50d51c36f
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-18 14:20:10 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function greeting(name, age)
+{
+    document.write("반가워요 " + name + 
+    " 당신의 나이는 " + age + "살이네요.")
+
+    document.write(`반가워요 ${name} 
+    당신의 나이는 ${age}살이네요.`)
+}
+
+greeting("RS", 22)
